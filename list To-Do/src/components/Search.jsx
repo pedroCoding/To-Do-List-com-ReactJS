95904450b38299bcec1253111a822d8f48a46b00
PEDRO <falecompedrodev@gmail.com>
2023-05-28 20:38:21 -0300

    Adicionando função "Busca"

---
+++ list To-Do/src/components/Search.jsx
@@ -0,0 +1,8 @@
+const Search = ({search, setSearch }) => {
+  return <div className="search">
+    <h2>Pesquisar</h2>
+    <input type="text" className="text" />
+  </div>
+}
+
+export default Search
