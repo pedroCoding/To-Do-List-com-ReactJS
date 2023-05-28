4ed1563a19269e0c1e5a6cba44036c63f8e515e4
PEDRO <falecompedrodev@gmail.com>
2023-05-27 21:42:44 -0300

    Correção de Código

--- list To-Do/src/App.jsx
+++ list To-Do/src/App.jsx
@@ -27,12 +27,14 @@ function App() {
     <h1>Lista de Tarefas</h1>
     <div className="todo-list">
       {todos.map((todo) => (
-        <div className="content">
-          <p>{todo.text}</p>
-          <p className="category">({todo.category})</p>
-          <div>
-            <button>Completar</button>
-            <button>x</button>
+        <div className="todo">
+          <div className="content">
+            <p>{todo.text}</p>
+            <p className="category">({todo.category})</p>
+            <div>
+              <button>Completar</button>
+              <button>x</button>
+            </div>
           </div>
         </div>
       ))}
