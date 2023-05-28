c857cc508888d3de944bf391d4bdea7e7a003e4c
PEDRO <falecompedrodev@gmail.com>
2023-05-28 20:46:13 -0300

    Adicionando funcionalidade pesquisar

--- list To-Do/src/components/Search.jsx
+++ list To-Do/src/components/Search.jsx
@@ -1,7 +1,7 @@
 const Search = ({search, setSearch }) => {
   return <div className="search">
     <h2>Pesquisar</h2>
-    <input type="text" className="text" />
+    <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Digite para pesquisar..." />
   </div>
 }
 
