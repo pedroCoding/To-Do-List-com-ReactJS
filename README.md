6316cdadd84c0181b775d0161c7af12f4cd0bf0a
PEDRO <falecompedrodev@gmail.com>
2023-05-28 21:03:09 -0300

    Alterações

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 Projeto To-Do List em tempo integral, feito com boas práticas de React JS com Vite
 
+<h1>Funcionalidades</h2>
+- Ferramenta de Busca
+- Ferramenta de Adição e Exclusão de tarefas
+- Ferramenta de Busca avançada
+- CRUD
 
 <h1>Proposta de extensão de Projeto</h1>
 - Linkar a um banco de dados local Mysql para o tornar prático, funcional e usual...
--- list To-Do/src/App.jsx
+++ list To-Do/src/App.jsx
@@ -29,6 +29,9 @@ function App() {
 
   const [search, setSearch] = useState("");
 
+  const [filter, setFilter] = useState("All");
+  const [sort, setSort] = useState("Asc");
+
   const addTodo = (text, category) => {
 
       const newTodos = [...todos, {
@@ -60,10 +63,13 @@ function App() {
   return(
     <div className="app">
       <h1>Lista de Tarefas</h1>
-      <Search search={search} setSearch={setSearch}/>
-      <Filter />
+      <Search search={search} setSearch={setSearch} />
+      <Filter filter={filter} setFilter={setFilter} />
       <div className="todo-list">
-        {todos.filter((todo) => todo.text.toLowerCase().includes(search.toLowerCase())).map((todo) => (
+        {todos
+        .filter((todo) => filter === "All" ? true : filter === "Completed" ? todo.isCompleted : !todo.isCompleted)
+        .filter((todo) => todo.text.toLowerCase().includes(search.toLowerCase()))
+        .map((todo) => (
           <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo}/>
         ))}
       </div>
